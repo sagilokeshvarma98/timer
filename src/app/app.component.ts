1d63057023c82f76d6ae3529898244a58be81938
sagilokeshvarma98 <sagilokeshvarma98@gmail.com>
2022-02-11 15:22:28 +0530

    '1stCommit'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { timeInterval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'timer';
+  time: number = 60;
+  a:any;
+  startTimer() {
+    this.a = setInterval(()=>{
+      if(this.time === 0) {
+        this.pauseTimer();
+      }
+      else
+      this.time = this.time - 1;
+    },100);
+  }
+
+  pauseTimer() {
+    clearInterval(this.a);
+  }
 }
